1e7e4d877b2b29cecb885d6601380c109e7badeb
cicec <clancysong@gmail.com>
2023-09-01 12:43:30 +0800

    feat: update

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -12,12 +12,16 @@ const IndexPage: FC = () => {
   const chartRef = useRef<Chart | null>(null);
 
   const [configuration, setConfiguration] = useState({
-    totalAssets: 50,
-    annualizedRate: 3,
-    income: 30,
+    numberOfYears: 30,
+    totalAssets: 10,
+    annualizedRate: 20,
+    income: 20,
     incomeIncreaseRate: 8,
     expense: 10,
     inflationRate: 4,
+    maximumIncome: 50,
+    minimumIncome: 0,
+    workingYears: 10,
   });
 
   useEffect(() => {
@@ -39,6 +43,10 @@ const IndexPage: FC = () => {
         datasets: [],
       },
       options: {
+        responsive: true,
+        interaction: {
+          intersect: false,
+        },
         scales: {
           y: {
             beginAtZero: true,
@@ -55,6 +63,16 @@ const IndexPage: FC = () => {
       return;
     }
 
+    const {
+      numberOfYears,
+      annualizedRate,
+      incomeIncreaseRate,
+      inflationRate,
+      maximumIncome,
+      minimumIncome,
+      workingYears,
+    } = configuration;
+
     const Datasets: {
       totalAssets: number[];
       passiveIncome: number[];
@@ -67,13 +85,9 @@ const IndexPage: FC = () => {
       expense: [configuration.expense],
     };
 
-    const numberOfYears = 30;
-    const needStopWorking = true;
-    const minimumIncome = 5;
-
-    Array.from({ length: numberOfYears }).forEach(() => {
-      const { annualizedRate, incomeIncreaseRate, inflationRate } = configuration;
-      const { totalAssets, passiveIncome, income, expense } = _.mapValues(
+    Array.from({ length: numberOfYears }).forEach((_value, index) => {
+      const year = index + 1;
+      const { totalAssets, income, expense } = _.mapValues(
         Datasets,
         dataset => _.last(dataset) as number
       );
@@ -84,8 +98,10 @@ const IndexPage: FC = () => {
       Datasets.passiveIncome.push(totalAssets * (annualizedRate / 100));
       Datasets.expense.push(expense * (1 + inflationRate / 100));
 
-      if (passiveIncome > expense && needStopWorking) {
+      if (year > workingYears) {
         Datasets.income.push(minimumIncome);
+      } else if (income >= maximumIncome) {
+        Datasets.income.push(maximumIncome);
       } else {
         Datasets.income.push(income * (1 + incomeIncreaseRate / 100));
       }
@@ -97,18 +113,30 @@ const IndexPage: FC = () => {
         {
           label: '总存款',
           data: Datasets.totalAssets,
+          fill: false,
+          cubicInterpolationMode: 'monotone',
+          tension: 0.4,
         },
-        {
-          label: '年收入',
-          data: Datasets.income,
-        },
+        // {
+        //   label: '年收入',
+        //   data: Datasets.income,
+        //   fill: false,
+        //   cubicInterpolationMode: 'monotone',
+        //   tension: 0.4,
+        // },
         {
           label: '被动收入',
           data: Datasets.passiveIncome,
+          fill: false,
+          cubicInterpolationMode: 'monotone',
+          tension: 0.4,
         },
         {
           label: '支出',
           data: Datasets.expense,
+          fill: false,
+          cubicInterpolationMode: 'monotone',
+          tension: 0.4,
         },
       ],
     };
@@ -133,10 +161,28 @@ const IndexPage: FC = () => {
       <div className={style.configuration}>
         <div className={style.row}>
           <Input
+            value={String(configuration.numberOfYears)}
+            type="number"
+            label="计算年限"
+            startContent={
+              <div className="pointer-events-none flex items-center">
+                <span className="text-default-400 text-small">共</span>
+              </div>
+            }
+            endContent={
+              <div className="pointer-events-none flex items-center">
+                <span className="text-default-400 text-small whitespace-nowrap">年</span>
+              </div>
+            }
+            onChange={bindChangeEvent('numberOfYears')}
+          />
+        </div>
+
+        <div className={style.row}>
+          <Input
             value={String(configuration.totalAssets)}
             type="number"
             label="现有存款"
-            placeholder="0.00"
             startContent={
               <div className="pointer-events-none flex items-center">
                 <span className="text-default-400 text-small">￥</span>
@@ -156,7 +202,6 @@ const IndexPage: FC = () => {
             value={String(configuration.annualizedRate)}
             type="number"
             label="年化利率"
-            placeholder="0.00"
             endContent={
               <div className="pointer-events-none flex items-center">
                 <span className="text-default-400 text-small whitespace-nowrap">%</span>
@@ -171,7 +216,6 @@ const IndexPage: FC = () => {
             value={String(configuration.income)}
             type="number"
             label="年收入（税后）"
-            placeholder="0.00"
             startContent={
               <div className="pointer-events-none flex items-center">
                 <span className="text-default-400 text-small">￥</span>
@@ -188,10 +232,29 @@ const IndexPage: FC = () => {
 
         <div className={style.row}>
           <Input
+            value={String(configuration.workingYears)}
+            type="number"
+            label="工作至"
+            labelPlacement="outside-left"
+            startContent={
+              <div className="pointer-events-none flex items-center">
+                <span className="text-default-400 text-small">第</span>
+              </div>
+            }
+            endContent={
+              <div className="pointer-events-none flex items-center">
+                <span className="text-default-400 text-small whitespace-nowrap">年</span>
+              </div>
+            }
+            onChange={bindChangeEvent('workingYears')}
+          />
+        </div>
+
+        <div className={style.row}>
+          <Input
             value={String(configuration.incomeIncreaseRate)}
             type="number"
-            label="年均收入增长速率"
-            placeholder="0.00"
+            label="年收入增长速率"
             endContent={
               <div className="pointer-events-none flex items-center">
                 <span className="text-default-400 text-small whitespace-nowrap">%</span>
@@ -203,10 +266,48 @@ const IndexPage: FC = () => {
 
         <div className={style.row}>
           <Input
+            value={String(configuration.maximumIncome)}
+            type="number"
+            label="年收入最高增长至"
+            labelPlacement="outside-left"
+            startContent={
+              <div className="pointer-events-none flex items-center">
+                <span className="text-default-400 text-small">￥</span>
+              </div>
+            }
+            endContent={
+              <div className="pointer-events-none flex items-center">
+                <span className="text-default-400 text-small whitespace-nowrap">万元</span>
+              </div>
+            }
+            onChange={bindChangeEvent('maximumIncome')}
+          />
+        </div>
+
+        <div className={style.row}>
+          <Input
+            value={String(configuration.minimumIncome)}
+            type="number"
+            label="最低限度年收入（税后）"
+            startContent={
+              <div className="pointer-events-none flex items-center">
+                <span className="text-default-400 text-small">￥</span>
+              </div>
+            }
+            endContent={
+              <div className="pointer-events-none flex items-center">
+                <span className="text-default-400 text-small whitespace-nowrap">万元</span>
+              </div>
+            }
+            onChange={bindChangeEvent('minimumIncome')}
+          />
+        </div>
+
+        <div className={style.row}>
+          <Input
             value={String(configuration.expense)}
             type="number"
             label="年支出"
-            placeholder="0.00"
             startContent={
               <div className="pointer-events-none flex items-center">
                 <span className="text-default-400 text-small">￥</span>
@@ -226,7 +327,6 @@ const IndexPage: FC = () => {
             value={String(configuration.inflationRate)}
             type="number"
             label="通胀率"
-            placeholder="0.00"
             endContent={
               <div className="pointer-events-none flex items-center">
                 <span className="text-default-400 text-small whitespace-nowrap">%</span>
